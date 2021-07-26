74db0b9f47910ee9a0a0f8bf60c45fcfdbf13195
Ron Kozan <kozan@computer.org>
2021-07-26 17:49:39 +0000

    changes made from the Theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     
+    // Check to ensure the principal value entered was greater than zero.
+    checkInput();
+
+    var interest = parseFloat(principal,10) * parseFloat(years,10) * parseFloat(rate, 10) / 100.0;
+    var year = new Date().getFullYear() + parseInt(years);
+    
+    var result = document.getElementById("result").value;
+
+    // Assemble the results text for display.
+    document.getElementById("result").innerHTML = "If you deposit <mark>$"
+        + principal + "</mark>,<br> at an interest rate of <mark>" + rate + 
+        "%</mark>, <br>you will receive an amount of <mark>$" + interest +
+        "</mark>, <br>in the year <mark>" + year + "</mark>.";
 }
-        
+function checkInput()
+{
+    var principal = document.getElementById("principal").value;
+
+    // check to ensure principal value is positive and non-negative
+    if (principal < 1){
+                alert("Enter a postive value.");
+                principal.focus();
+                return false;
+     } 
+     return true; 
+}
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+
+    document.getElementById("rate_val").innerText = rateval;
+}       
